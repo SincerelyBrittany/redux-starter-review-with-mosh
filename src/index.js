618fb4b4d17d05b39f1a13dca44ddccdd8cb17b3
SincerelyBrittany <sincerelybr@gmail.com>
2021-09-17 23:58:53 -0400

    updates index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-import store from "./store";
-console.log(store);
-import * as actions from "./actionTypes"; //-- MOVE TO ACTION CREATORS FILE
-import { ADD_BUG } from "./actionCreators";
+import store from "./customStore";
+import * as actions from "./actions";
 
-const unsubscribe = store.subscribe(() => {
-  console.log("Store changed!", store.getState());
-});
-
-console.log(store.getState());
-
-store.dispatch(ADD_BUG("Bug 1"));
-
-console.log(store.getState());
-unsubscribe();
-store.dispatch({
-  type: actions.REMOVE_BUG,
-  payload: {
-    id: 1,
-  },
-});
-
-console.log(store.getState());
+//console.log(store.dispatch(actions.ADD_BUG("Bug 1")));
+store.dispatch(actions.ADD_BUG("Bug 1"))
