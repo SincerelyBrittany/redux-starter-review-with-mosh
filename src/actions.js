e7c870280e6c876f2af78322664295eda17e4db5
Brittany <sincerelybr@gmail.com>
2021-09-20 17:25:58 -0400

    Updates actions and store example

--- src/actions.js
+++ src/actions.js
@@ -1,2 +1,22 @@
-export const ADD_BUG = "ADD_BUG";
-export const REMOVE_BUG = "REMOVE_BUG";
+import * as actions from "./ActionTypes";
+
+export const ADD_BUG = (description) => ({
+    type: actions.ADD_BUG,
+    payload: {
+      description: "Bug1",
+    },
+  });
+
+
+//   export const REMOVE_BUG = (description) => ({
+//     type: actions.ADD_BUG,
+//     payload: {
+//       description: "Bug1",
+//     },
+//   });
+
+
+export const RESOLVE_BUG = id => ({
+    type: actions.RESOLVE_BUG,
+    payload:  {id:id}
+});
